9528474cff6ba4ba32098591e986324ea974835c
Snecegamer <matheusfernandessalomao@gmail.com>
2026-08-26 21:43:21 -0300

    Add files via upload

---
+++ home.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, ... }:
+let
+  dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  configs = {
+   nvim = "nvim";
+   rofi = "rofi";
+   i3 = "i3";
+   polybar = "polybar";
+   picom = "picom";
+  };
+in
+{
+ home.username = "snece";
+ home.homeDirectory = "/home/snece";
+ programs.git.enable = true;
+ home.stateVersion = "26.05";
+ programs.bash = {
+ enable = true;
+ bashrcExtra = "fastfetch";
+ shellAliases = {
+   btw = "echo i use NixOS btw";
+  };
+ };
+
+ xdg.configFile = builtins.mapAttrs (name: subpath: {
+   source =  create_symlink "${dotfiles}/${subpath}";
+   recursive = true;
+ }) configs;
+
+ home.packages = with pkgs; [
+  steam
+  brave
+  picom
+  flameshot
+  feh
+  vlc
+  xwallpaper
+  rofi
+  flatpak
+  onlyoffice-desktopeditors
+  thunar
+  gvfs
+  fastfetch
+  neovim
+  ripgrep
+  nil
+  nixpkgs-fmt
+  nodejs
+  gcc
+  unrar
+  unzip
+  vscode
+  obsidian
+ ];
+}
